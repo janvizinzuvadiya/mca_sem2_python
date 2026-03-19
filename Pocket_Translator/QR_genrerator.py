86891130d262b73f730a9d827a224de909880625
janvizinzuvadiya <zinzuvadiyajanvi4@gmail.com>
2026-03-19 13:14:31 +0530

    QR code Generated

--- Pocket_Translator/QR_genrerator.py
+++ Pocket_Translator/QR_genrerator.py
@@ -1,1 +1,15 @@
-https://gist.github.com/janvizinzuvadiya/778f3e7f8bb052a972d6e656ab40421c
+import qrcode # pip install qrcode[pil]
+
+# 1. PASTE YOUR LINK HERE
+my_link = "https://gist.github.com/janvizinzuvadiya/778f3e7f8bb052a972d6e656ab40421c" 
+
+# 2. Setup the QR style
+qr = qrcode.QRCode(version=1, box_size=10, border=4)
+qr.add_data(my_link)
+qr.make(fit=True)
+
+# 3. Create and save the image
+img = qr.make_image(fill_color="black", back_color="white")
+img.save("my_poster_qr.png")
+
+print("Success! 'my_poster_qr.png' is ready to be printed and glued to your chart.")
