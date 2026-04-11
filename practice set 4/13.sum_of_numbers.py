39a61ea065c5ef035b3c25661abad9b17157e87f
janvizinzuvadiya <zinzuvadiyajanvi4@gmail.com>
2026-04-11 08:45:30 +0530

    practice set 4 complete

--- practice set 4/13.sum_of_numbers.py
+++ practice set 4/13.sum_of_numbers.py
@@ -8,10 +8,31 @@ def print_header(title):
     print("=" * 50)
 
 def main():
-    print_header("Vowel and Consonant Counter")
-    print("Let's count the vowels and consonants in your string.")
+    print_header("Sum of Numbers")
+    print("Let's find the sum of the numbers you enter.")  
     try:
-        num = int(input("👉 Please enter : "))
+        print('How many numbers you want to enter?')
+        n = int(input("👉 Please enter : "))
+        
     except ValueError:
-        print("\n❌ Error: Please enter a valid string!")
+        print("\n❌ Error: Please enter a valid Number!")
         return
+
+    sum = 0
+    for i in range(n):
+        print('Enter Actual Number')
+        num = int(input(f"👉 Please Enter Number {i+1} : "))
+        sum += num
+
+    print(f"The sum of {n} numbers is {sum}")
+
+    print("\n" + "=" * 50)
+    print("Thank you for using the Sum of Numbers calculator!")
+    print("=" * 50)
+    time.sleep(1)
+    return
+
+if __name__ == "__main__":
+    main()
+
+    
--- practice set 4/7.count_vowel_const.py
+++ practice set 4/7.count_vowel_const.py
@@ -22,7 +22,7 @@ def main():
     print("-" * 50)
     vowels = 0
     consonants = 0
-    for char in string:
+    for char in string: 
         if(char.lower() in "aeiou"):
             vowels += 1
         elif(char.isalpha()):
